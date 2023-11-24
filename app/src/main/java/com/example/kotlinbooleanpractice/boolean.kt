a66c6802bcc0792f9f2d334adbd8243c373959ca
DESKTOP-4L7GJRP\Orax <saad.oraxtech@gmail.com>
2023-11-24 17:59:04 +0500

    First commit

---
+++ app/src/main/java/com/example/kotlinbooleanpractice/boolean.kt
@@ -0,0 +1,33 @@
+package com.example.kotlinbooleanpractice
+
+fun Bool() {
+ val myTrue:Boolean = true
+ val myFalse : Boolean = false
+
+ println(myTrue || myFalse)  //True
+ println(myTrue && myFalse)  //False
+ println(!myTrue)            //False
+}
+
+fun main() {
+ //Bool()
+ inputhandling()
+}
+
+fun inputhandling(){
+ println("Enter your Age")
+ //val enteredValue = readln()
+ var age = readln().toInt()
+ if (age in 18..39)
+ {
+  println("You can enter into the Club")
+ }
+ else if(age>=40)
+ {
+  println("You are not allowed to enter.PLZ GO HOME :)")
+ }
+ else
+ {
+  println("You are not allowed to enter")
+ }
+}
